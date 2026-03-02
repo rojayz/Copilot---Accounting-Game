69972e1e45b9e08e2454507dbc7eac3c172c0131
rojayz <reed.b.norris@gmail.com>
2026-03-02 15:56:45 -0500

    Update coa.js

--- data/coa.js
+++ data/coa.js
@@ -1,1 +1,21 @@
+export const COA = [
+  { id:"cash", name:"Cash", type:"asset", normal:"D" },
+  { id:"ar", name:"Accounts Receivable", type:"asset", normal:"D" },
+  { id:"supplies", name:"Supplies", type:"asset", normal:"D" },
+  { id:"equip", name:"Equipment", type:"asset", normal:"D" },
 
+  { id:"ap", name:"Accounts Payable", type:"liability", normal:"C" },
+  { id:"notes", name:"Notes Payable", type:"liability", normal:"C" },
+  { id:"unearned", name:"Unearned Revenue", type:"liability", normal:"C" },
+
+  { id:"cs", name:"Common Stock", type:"equity", normal:"C" },
+  { id:"re", name:"Retained Earnings", type:"equity", normal:"C" },
+  { id:"div", name:"Dividends", type:"equity", normal:"D" },
+
+  { id:"rev", name:"Service Revenue", type:"revenue", normal:"C" },
+
+  { id:"rentExp", name:"Rent Expense", type:"expense", normal:"D" },
+  { id:"salExp", name:"Salaries Expense", type:"expense", normal:"D" },
+  { id:"supExp", name:"Supplies Expense", type:"expense", normal:"D" },
+  { id:"depExp", name:"Depreciation Expense", type:"expense", normal:"D" },
+];
